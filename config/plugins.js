38a263f8a69da765237caf9f44a1c552b0e83c71
Derrick Mehaffy <derrickmehaffy@gmail.com>
2025-02-20 09:59:11 -0800

    move plugin config to base

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,17 @@
-module.exports = () => ({});
+module.exports = {
+  upload: {
+    enabled: true,
+    config: {
+      breakpoints: {
+        // Note: xxlarge to small sizes follow mpm-web/public/stylesheets/settings.scss
+        zoom: 1920,
+        xxlarge: 1440,
+        xlarge: 1280,
+        large: 1024,
+        medium: 768,
+        small: 667,
+        thumbnail: 156,
+      },
+    },
+  },
+};
